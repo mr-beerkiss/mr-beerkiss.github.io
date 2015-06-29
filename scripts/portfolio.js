e7f54afb6d641c16c080c9eb878a990cffd22a39
Darren Beukes <darren.beukes@gmail.com>
2015-06-29 23:52:37 +0100

    Improvements to portfolio section
    1. Moved Portfolio JS function into closure
    2. Added loading cog animation while iFrame is loaded
    3. Improved border around iFrame

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,41 +1,58 @@
-var iFrameOverlay;
-var iFrameContainer;
-var currentiFrame;
+var portfolio = window.portfolio || (function(window, document, undefined) {
+    var iFrameOverlay;
+    var iFrameContainer;
+    var iFrameLoader;
 
-function openiFrame(url, width, height) {
-    if ( !iFrameContainer || !iFrameOverlay ) {
-        iFrameOverlay = document.querySelector(".beerkiss-iframe-overlay");
-        iFrameContainer = document.querySelector(".beerkiss-iframe-container");
+    var currentiFrame;
+
+    function openiFrame(url, width, height) {
+        if ( !iFrameContainer || !iFrameOverlay ) {
+            iFrameOverlay = document.querySelector(".beerkiss-iframe-overlay");
+            iFrameContainer = document.querySelector(".beerkiss-iframe-container");
+            iFrameLoader = document.querySelector(".beerkiss-iframe-loading");
+        }
+
+        iFrameOverlay.classList.remove("hidden");
+        
+
+        if ( !url ) {
+            return;
+        }
+
+        width = width || 720;
+        height = height || 720;
+
+        var iFrame = document.createElement("iframe");
+
+        iFrame.src = url;
+        iFrame.width = width;
+        iFrame.height = height;
+        iFrame.className = "beerkiss-iframe";
+      
+        iFrame.onload = function() {
+            iFrameLoader.classList.add("hidden");
+        };
+
+
+        currentiFrame = iFrame;
+
+        iFrameContainer.appendChild(currentiFrame);
     }
 
-    iFrameOverlay.classList.remove("hidden");
+    function destroyiFrame() {
+        
+        iFrameContainer.removeChild(currentiFrame);
 
-    if ( !url ) {
-        return;
+        currentiFrame = undefined;
+
+        iFrameOverlay.classList.add("hidden");
+        iFrameLoader.classList.remove("hidden");
+
     }
 
-    width = width || 720;
-    height = height || 720;
+    return {
+        openiFrame: openiFrame,
+        destroyiFrame: destroyiFrame
+    };
+})(window, document);
 
-    var iFrame = document.createElement("iframe");
-
-    iFrame.src = url;
-    iFrame.width = width;
-    iFrame.height = height;
-    iFrame.className = "beerkiss-iframe";
-  
-
-    currentiFrame = iFrame;
-
-    iFrameContainer.appendChild(currentiFrame);
-}
-
-function destroyiFrame() {
-    
-    iFrameContainer.removeChild(currentiFrame);
-
-    currentiFrame = undefined;
-
-    iFrameOverlay.classList.add("hidden");
-
-}
